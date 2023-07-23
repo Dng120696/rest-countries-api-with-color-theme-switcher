9b65b07cbef1de5ea141bf3abc58d44420737774
Dng16 <dongyang0016@gmail.com>
2023-07-23 10:49:21 +0800

    final commit

--- src/script.js
+++ src/script.js
@@ -1,26 +1,4 @@
-"use strict";
-
-const menu = document.querySelector(".menu");
-const lightMode = document.querySelector(".light-mode");
-const darkMode = document.querySelector(".dark-mode");
-const menuTitle = document.querySelector(".menu-title");
-class NavMenu {
-  constructor() {
-    menu.addEventListener("click", this._ligthMode.bind(this));
-  }
-  _ligthMode() {
-    lightMode.classList.toggle("hidden");
-    darkMode.classList.toggle("hidden");
-
-    const isDarkMode = darkMode.classList.contains("hidden");
-    document.querySelector("body").classList.toggle("light-mode", !isDarkMode);
-    menuTitle.textContent = !isDarkMode ? "Dark Moded" : "Light Mode";
-    localStorage.setItem(
-      "modePreference",
-      !isDarkMode ? "light-mode" : "dark-mode"
-    );
-  }
-}
+import { darkMode, lightMode, menuTitle, NavMenu } from "./nav.js";
 
 const countrydata = document.querySelector(".country-data");
 const inputSearch = document.getElementById("input-search");
